c956805631dcf874059d7859a6e6fd48f572bafb
simplybychris <draytv55@gmail.com>
2025-08-30 09:54:16 +0200

    feat: Add divide function

--- calculator.py
+++ calculator.py
@@ -1,3 +1,9 @@
+def divide(a, b):
+    """This function divides two numbers."""
+    if b == 0:
+        raise ValueError("Cannot divide by zero!")
+    return a / b
+
 def multiply(a, b):
     """This function multiplies two numbers."""
     return a * b
