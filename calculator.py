e04afc641ec2131c277fd0fdc91a31aac5d3d64b
luka7icja <alicja_luka@op.pl>
2025-09-05 16:30:37 +0200

    Dodano potęgowanie

--- calculator.py
+++ calculator.py
@@ -10,7 +10,13 @@ def add(a, b):
     """This function adds two numbers."""
     return a + b
 
+def power(a,b):
+    """This function calculates the exponent of a number."""
+    return a ** b
+
 if __name__ == "__main__":
     print("Welcome to the simple calculator!")
     result = add(2, 3)
     print(f"2 + 3 = {result}")
+    result2 = power(2, 3)
+    print(result2)
